3111a4306dcddf627b5daaf905bd67d2e8926ca1
Carlos Eduardo Serpa Brito <61998196+CarlosEduardoSB@users.noreply.github.com>
2022-03-11 13:48:46 -0300

    Projeto Tela Login

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tela Login</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="CSS/estilo.css">
+
+</head>
+<body>
+	<form method="POST">
+		<input type="email">
+		<input type="password">
+		<input type="submit">
+		
+	</form>
+
+</body>
+</html>
